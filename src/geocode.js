dffdeda23efa687f66a2d816c932c427ced953c7
Brad Korman <brad.korman@remarque-creative.com>
2020-08-18 21:19:24 -0400

    debugging geocode issue x2

--- src/geocode.js
+++ src/geocode.js
@@ -6,11 +6,11 @@ const mb = process.env.MAPBOX_API;
 const geoCode = (address, callback) => {
   const url =  `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mb}`;
   request({ url, json: true}, (error, { body }) => {
+    console.log(error, body);
     if (error) {
       callback('Unable to connect', undefined)
     } else if (!body.features || body.features.length === 0) {
       callback('Unable to find location', undefined)
-      console.log(body);
     } else {
       callback(undefined, {
          lat: body.features[0].center[1],
